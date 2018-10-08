e25fe2f5fe4c0eb3a16cdb24168ad29d8d630bf0
Evan Lemley <elemley@uco.edu>
2018-10-08 13:52:23 -0500

    update

--- gauss1.py
+++ gauss1.py
@@ -10,32 +10,32 @@ def augment(a,b):
     ab = np.c_[a, b]  # we now should have the augmented form - this is just a python numpy library function
     return ab
 
-def get_sum(ab,x,order,i):
-    #stuff
+def get_sum(ab,x,i):
+    summ = 0
+    n = len(ab)
+    for j in range(i+1,n):
+        summ+=ab[i,j]*x[j]
     return 1
 
-
 def main():
-
     #this is an example from class
-    #a_lst = [[0.143, 0.357,2.01], [-1.31, 0.911, 1.99],[11.2, -4.30, -0.605]]   #this is a list of lists
-    #a_lst = [[0.0, 5.0,-1.0], [-4.0, 2.0, 3.0],[4.0, 3.0, 3.0]]   #this is a list of lists
-
     a_lst = [[1, -3, 1],[0, 1, -3],[0, 0,1]]   #this is a list of lists
     a = np.array(a_lst) #this makes a_lst an 2D array
-
-
-    #b_lst = [-5.173, -5.458,4.415]
-    #b_lst = [-5.0, -17.0,7.0]
     b_lst = [4, 5,-2]
     b= np.array(b_lst)
+    n = len(ab)         #number of rows in ab
+    x[n-1] = b[n-1]/a[n-1,n-1]
+    for i in range(n-2,-1,-1):
+
+
+
 
     #print a,b
     #a = np.empty(shape=(n,n))
     #b = np.empty(shape=(n,1))
 
     ab = augment(a,b)   #augment the b RHS vector on the right side of a
-    n = len(ab)         #number of rows in ab
+
     print(ab)
 
     print(ab[0,0])
@@ -51,3 +51,7 @@ if __name__ == '__main__':
 
 
 
+# a_lst = [[0.143, 0.357,2.01], [-1.31, 0.911, 1.99],[11.2, -4.30, -0.605]]   #this is a list of lists
+# a_lst = [[0.0, 5.0,-1.0], [-4.0, 2.0, 3.0],[4.0, 3.0, 3.0]]   #this is a list of lists
+# b_lst = [-5.173, -5.458,4.415]
+# b_lst = [-5.0, -17.0,7.0]
